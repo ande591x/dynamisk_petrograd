6667513cdbe27ec6d357eab272ca2cc30737f666
anders berenth <anders berenth>
2017-02-23 12:41:49 +0100

    rettet fejl så alle henvisninger er rigtige

--- script.js
+++ script.js
@@ -20,9 +20,22 @@ function visprodukt(produkt) {
     klon.querySelector(".data_navn").innerHTML = produkt.navn;
     klon.querySelector(".data_pris").innerHTML = produkt.pris;
 
-    var rabatpris = math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
 
-    klon.querySelector("data_rabatpris").innerHTML = rabatpris;
+    klon.querySelector(".data_rabat").innerHTML = rabatpris;
+
+    klon.querySelector(".data_billede").src = "/imgs/medium/" + produkt.billede + "-md.jpg";
+
+    if (produkt.udsolgt == false) {
+        // produktet er ikke udsolgt
+        // udsolgtekst skal fjernes
+
+        var udsolgtekst = klon.querySelector(".udsolgtekst");
+        udsolgtekst.parentNode.removeChild(udsolgtekst);
+
+    } else {
+        klon.querySelector(".pris").classList.add("udsolgt");
+    }
 
     // append klon til .produkt_liste
 
